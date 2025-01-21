cb1f7c05c2583535b5c6c430c8b329f202646d23
Aygor7 <ajgor.lewandowski09@gmail.com>
2025-01-21 15:58:16 +0100

    Add files via upload

---
+++ Drugi tydzień/Zadanie1/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  
+  <HelloWorld msg="Galeria zdjęć" aha="Krajobraz Japonii" source="src/assets/zdj1.jpg"/>
+  <HelloWorld source="src/assets/zdj2.jpg"/>
+  <HelloWorld source="src/assets/zdj3.jpg"/>
+  <HelloWorld source="src/assets/zdj4.jpg"/>
+  <HelloWorld source="src/assets/zdj5.jpg"/>
+  <HelloWorld source="src/assets/zdj6.jpg"/>
+  <HelloWorld source="src/assets/zdj7.jpg"/>
+  <HelloWorld source="src/assets/zdj8.jpg"/>
+  <HelloWorld source="src/assets/zdj9.jpg"/>
+  <HelloWorld source="src/assets/zdj10.jpg"/>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ Drugi tydzień/Zadanie1/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+  aha: String,
+  source: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <h2>{{ aha }}</h2>
+
+  <img id="xd" :src="source" alt="Image"/>
+
+</template>
+
+<style scoped>
+#xd {
+  width: 400px;
+  height: 300px;
+  float: left;
+  border: 5px dashed pink;
+  margin: 10px;
+}
+</style>
